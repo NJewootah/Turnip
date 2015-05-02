be5f8ed9f05890e58a358b1defe9bb1c48ab172a
Neermal <jneermal@gmail.com>
2015-05-02 03:25:45 +0100

    Implemented page object methods

---
+++ Spec/step_definitions/search_term_steps.rb
@@ -0,0 +1,15 @@
+steps_for :search_term do
+	step "I am logged in" do
+		@app.login.visit
+		@app.login.login('ninistudies@gmail.com', 'Pass123w')
+		@app.home.visit
+	end
+	
+	step "I search for :term" do |term|
+		@app.taskbar.search term
+	end
+	
+	step "I should see a list of results relevant to :term" do |term|
+		@app.results.check_relevant term
+	end
+end
