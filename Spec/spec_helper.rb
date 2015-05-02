5b6b7526ead452a42ee2f159bc9a9d10b33e37ee
Neermal <jneermal@gmail.com>
2015-05-02 03:15:12 +0100

    Loaded pages and steps and added requirements

---
+++ Spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'watir'
+require 'pry'
+
+Dir.glob("spec/**/*steps.rb") { |f| load f}
+Dir.glob("spec/lib/**/*.rb") { |f| load f}
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
