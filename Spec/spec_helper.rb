a2513b3a6767d51a305fc9cdbdd883d6c5adec03
Neermal <jneermal@gmail.com>
2015-05-06 00:45:57 +0100

    Loaded generic.rb prior to all page objects

--- Spec/spec_helper.rb
+++ Spec/spec_helper.rb
@@ -2,9 +2,11 @@ require 'rspec'
 require 'watir'
 require 'pry'
 
-Dir.glob("spec/**/*steps.rb") { |f| load f}
-Dir.glob("spec/lib/**/*.rb") { |f| load f}
+Dir.glob("spec/features/support/**/generic.rb") { |f| load f}
 Dir.glob("spec/features/support/**/*.rb") { |f| load f}
+
+Dir.glob("spec/lib/**/*.rb") { |f| load f}
+Dir.glob("spec/**/*steps.rb") { |f| load f}
 
 RSpec.configure do |config|
   config.color = true
