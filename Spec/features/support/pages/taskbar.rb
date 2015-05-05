8903e21471cc140784525c1070f53776b2a053a9
Neermal <jneermal@gmail.com>
2015-05-06 00:39:32 +0100

    Added logout method and modified search method

--- Spec/features/support/pages/taskbar.rb
+++ Spec/features/support/pages/taskbar.rb
@@ -1,6 +1,11 @@
 class Taskbar < Generic
   def search(term)
-    @browser.input(id: 'masthead-search-term').send_keys term
-    @browser.input(id: 'masthead-search-term').send_keys :enter
+    @browser.text_field(id: 'q').set term
+    @browser.text_field(id: 'q').set :enter
+  end
+
+  def logout()
+    @browser.div(text: 'Account Settings').click
+    @browser.input(value: 'Log out').click
   end
 end
