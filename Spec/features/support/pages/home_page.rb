da453e8ae15908ff146206c192e49c1d36c11e14
Neermal <jneermal@gmail.com>
2015-05-06 00:36:40 +0100

    Added new visit and check_status_exist methods

--- Spec/features/support/pages/home_page.rb
+++ Spec/features/support/pages/home_page.rb
@@ -1,5 +1,10 @@
 class HomePage < Generic
   def visit()
-    @browser.goto 'http://youtube.com'
+    @browser.div(class: '_5ah-').wait_until_present
+    @browser.div(class: '_5ah-').click
+  end
+
+  def check_status_exist(message)
+    @browser.div(class: '_5pbx userContent').p(text: message).wait_until_present
   end
 end
