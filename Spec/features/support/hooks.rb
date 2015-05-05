70454b4e60cf5e638855d9ed591d545e6679a7cf
Neermal <jneermal@gmail.com>
2015-05-06 00:31:55 +0100

    Added before and after hook for status feature

--- Spec/features/support/hooks.rb
+++ Spec/features/support/hooks.rb
@@ -8,4 +8,24 @@ RSpec.configure do |config|
   config.after(:type => :feature) do
     @browser.close
   end
+
+  # Status Feature Hook
+  config.before(:status => true) do
+    @app.login.visit
+    @app.login.login 'Nini'
+    @app.taskbar.search 'Neermal Tests'
+    @app.friend.send_friend_request
+    @app.taskbar.logout
+    @app.login.visit
+    @app.login.login 'Neermal'
+    @app.friend_request.visit
+    @app.friend_request.approve('Nini Tests')
+    @app.taskbar.logout
+  end
+
+  config.after(:status => true) do
+    @app.login.login 'Nini'
+    @app.taskbar.search 'Neermal Tests'
+    @app.friend.unfriend
+  end
 end
