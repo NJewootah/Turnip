2befc969212e04632a4b82a5b913d963e84dc960
Neermal <jneermal@gmail.com>
2015-05-06 00:51:28 +0100

    Removed redundant files

--- Spec/features/dirty/search_term.feature
+++
@@ -1,8 +0,0 @@
-Feature: Search Term
-
-  @search_term
-  Scenario: Should allow users to search for videos
-    Given I am logged in
-    When I search for Ruby
-    Then I should see a list of results relevant to Ruby
-    
--- Spec/step_definitions/search_term_steps.rb
+++
@@ -1,9 +0,0 @@
-steps_for :search_term do
-  step "I search for :term" do |term|
-    @app.taskbar.search term
-  end
-  
-  step "I should see a list of results relevant to :term" do |term|
-    @app.results.check_relevant term
-  end
-end
