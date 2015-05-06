1ea0f0acb57f5b72cb9c96543a94e5e7e6b71bf4
Neermal <jneermal@gmail.com>
2015-05-06 01:03:35 +0100

    Refactored add friend and unfriend steps

---
+++ Spec/lib/hook_methods.rb
@@ -0,0 +1,20 @@
+module HookMethods
+  def add_friend(user, friend)
+    @app.login.visit
+    @app.login.login user
+    @app.taskbar.search friend + ' Tests'
+    @app.friend.send_friend_request
+    @app.taskbar.logout
+    @app.login.visit
+    @app.login.login friend
+    @app.friend_request.visit
+    @app.friend_request.approve user + ' Tests'
+    @app.taskbar.logout
+  end
+
+  def unfriend(user, friend)
+    @app.login.login user
+    @app.taskbar.search friend + ' Tests'
+    @app.friend.unfriend
+  end
+end
