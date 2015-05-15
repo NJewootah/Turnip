93188aef821c7d8791afadfd0b4a9a016d0e97b8
Neermal <jneermal@gmail.com>
2015-05-15 02:04:50 +0100

    Added login session method to return boolean

--- Spec/features/support/pages/taskbar.rb
+++ Spec/features/support/pages/taskbar.rb
@@ -4,6 +4,14 @@ class Taskbar < Generic
     @browser.text_field(id: 'q').set :enter
   end
 
+  def login_session()
+    if @browser.div(id: 'pagelet_bluebar').present?
+      logout
+    else
+      false
+    end
+  end
+
   def logout()
     @browser.div(text: 'Account Settings').click
     @browser.input(value: 'Log out').click
