13ab0f5704f57d29595e9dedd58a3a0467d50234
Neermal <jneermal@gmail.com>
2015-05-15 02:08:30 +0100

    Added yaml requirement and loaded timeline post class earlier

--- Spec/spec_helper.rb
+++ Spec/spec_helper.rb
@@ -1,15 +1,17 @@
 require 'rspec'
 require 'watir'
 require 'pry'
+require 'yaml'
 
 Dir.glob("spec/features/support/**/generic.rb") { |f| load f}
 Dir.glob("spec/features/support/**/*.rb") { |f| load f}
 
+Dir.glob("spec/lib/**/timeline_post.rb") { |f| load f}
 Dir.glob("spec/lib/**/*.rb") { |f| load f}
+
 Dir.glob("spec/**/*steps.rb") { |f| load f}
 
 RSpec.configure do |config|
-  config.include HookMethods
   config.color = true
   config.tty = true
   config.formatter = :documentation
